eb0196ce3a0245ea1592e94139f9ca98ea989f76
SyonR12 <syonr11@gmail.com>
2024-07-24 14:20:52 -0400

    Timer

--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,9 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+function displayTime() {
+    time++;
+    counterElement.innerHTML = time;
+}
+
+setInterval(displayTime, 1000);
